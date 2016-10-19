2f88a8853248f55bf1f14d5f3326568ed94f5cc8
Rodrigo Rivera <Rodrigo_Rivera@Rodrigo-2.local>
2016-10-19 10:40:46 -0500

    comentado

--- proyectobueno.cpp
+++ proyectobueno.cpp
@@ -9,10 +9,13 @@
 using namespace std;
 
 class cliente;
-class caja;
+class cajero;
 
 
-
+/*
+*
+* Definir una clase que emule a un cliente 
+*/
 class cliente {
     
 
@@ -25,6 +28,9 @@ public:
     cliente(int id, string tipo, string hora, string tiemOp);
     cliente( void );
 
+/*
+* atributos de la clase 
+*/
 private:
     int idCliente;
     string tipoCliente;
@@ -35,43 +41,69 @@ private:
 
 
 };
-
+/*
+*   constructor de la clase que no recibe parametros
+*/
 cliente::cliente( void ){
-    cout << "Se está creando un cliente" << endl; 
+    // cout << "Se está creando un cliente" << endl; 
     idCliente = -1;  
 }
-
+/*
+*   constructor de la clase que recibe parametros
+* @param id  su identificador 
+* @param tipo que tipo de cliente es 
+* @param hora en que llega el cliente 
+* @param tiem0p que tarda
+*/
 cliente::cliente( int id, string tipo, string hora, string tiemOp)
 {
-    cout << "Se está creando un cliente" << endl;
+    //cout << "Se está creando un cliente" << endl;
     idCliente = id;
     tipoCliente = tipo;
     horaLlegada = hora;
     tiempoOperacion = tiemOp;
 }
  
+/*
+* @return id del cleinte
+*/
 int cliente::getId( void )
 {
     return idCliente;
 }
 
+/*
+* @return tipo de clinte
+*/
 string cliente::getTipoCliente ( void )
 {
     return tipoCliente;
 }
 
+/*
+* @return hora en que llego  el cliente
+*/
 string cliente::getHoraLlegada ( void )
 {
     return horaLlegada;
 }
 
+/*
+* @return tiempo que tarda el cliente en realizar la operacion 
+*/
 string cliente::getTiempoOp ( void )
 {
     return tiempoOperacion;
 }
 
-class caja {
+/*
+* clase que modela el cajero que va contener clientes
+*/
+class cajero {
 
+/*
+* metodos de la clase 
+*/
 public:
     int getId( void );
     int getTiempoEspera( void );
@@ -80,138 +112,99 @@ public:
     void masT( int tiem );
     cliente getU ( void );
     int tam ( void );
-    caja (int id, int tiempoEspera);
+    cajero (int id, int tiempoEspera);
     list<cliente> getListaPersonas( void );
+    
+
+/*
+* atributos de la clase
+*/
 private:
-    int idCaja;
+    int idcajero;
     int tiempoDeEspera;
+    
     list<cliente> listaPersonas;
 };
 
-caja::caja( int id, int tiempoEspera )
+/*
+*  contructor de la clase 
+* @param id numero de cajero
+* @param tiempoEspera tiempor que va a tardar un cliente 
+
+*/
+cajero::cajero( int id, int tiempoEspera )
 {
-    cout << "Se está creando una caja" << endl;
-    idCaja = id;
+    //cout << "Se está creando una cajero" << endl;
+    idcajero = id;
     tiempoDeEspera = tiempoEspera;
 
 }
-
-int caja::getId ( void )
+/*
+* 
+* @return regresa el numero de el cajero
+*/
+int cajero::getId ( void )
 {
-    return idCaja;
+    return idcajero;
 }
 
-int caja::getTiempoEspera( void )
+/*
+* @return un tiempo de espera
+*/
+int cajero::getTiempoEspera( void )
 {
     return tiempoDeEspera;
 }
-
-list<cliente> caja::getListaPersonas ( void )
+/*
+* @return la lista que contiene todos los clientes
+*/
+list<cliente> cajero::getListaPersonas ( void )
 {
     return listaPersonas;
 }
 
-void caja::add( cliente cl)
+/*
+* 
+*  @param agrega a un cliente a la 
+*/
+void cajero::add( cliente cl)
 {
     listaPersonas.push_front(cl);
+ 
 }
 
-void caja::masT( int tiem )
+/*
+*  @param aumenta al tiempo de espero el timepo que tarda 
+*/
+void cajero::masT( int tiem )
 {
     tiempoDeEspera += tiem;
 }
-
-cliente caja::getU ( void )
+/*
+* @return regrelsa 
+*/
+cliente cajero::getU ( void )
 {
     return listaPersonas.front();
 }
-
-int caja::tam ( void )
+/*
+* @return regresa el tamaño de la lista 
+*/
+int cajero::tam ( void )
 {
     return listaPersonas.size();
 }
 
-  /*
-  * mostrar los elementos de la lista
-  */
-/*void caja::mostrarLista( void ){
-     list<string>::iterator it = listaPersonas.begin();
-    cout <<"-------"<<idCajero<<endl;
-    while( it != listaPersonas.end() ){
-      
-      cout <<"" << *it++ << endl;
 
-    }
-    cout <<"*******"<<endl;
-  }
-
-  /*
-  * 
-  */
-/*int caja::scarPromedio( void ){
-
-    return 1;
-  }
-
-
-  /*
-  * escribir un arichivo txt con el id de cada clase
-  */
-/*void caja::escribir(){
-    ofstream fichero( "test-"+to_string(idCajero)+".txt");
-    list<string>::iterator it = listaPersonas.begin();
-    while( it != listaPersonas.end() ){
-      fichero <<*it++<<endl; // escibe e incrementa 
-    }
-    fichero.close();
-  }*/
-
-  /*
-  * muestra orden por  tipo de cliente  
-  * con un pequeño formato en consola
-  */
-/*void caja::mostrar(){
-    cout<<"----clientes: premium"<<endl;
-    list<string>::iterator it = listaPersonas.begin();
-    while( it != listaPersonas.end() ){
-        string p = personaCola.substr(2,1);
-        if( p == "P"){
-          cout<<*it<<endl;
-        }  
-      it++;
-    }
-    fichero.close();
-    
-
-
-    cout<<"----clientes: normales"<<endl;
-    list<string>::iterator it = listaPersonas.begin();
-    while( it != listaPersonas.end() ){
-        string p = personaCola.substr(2,1);
-        if( p == "C"){
-          cout<<*it<<endl;
-        }  
-      it++;
-    }
-
-    cout<<"----clientes: no clientes"<<endl;
-    
-    list<string>::iterator it = listaPersonas.begin();
-    while( it != listaPersonas.end() ){
-        string p = personaCola.substr(2,1);
-        if(  == "N"){
-          cout<<*it<<endl;
-        }  
-      it++;
-    }
-  }*/
-
-caja getMenorT( list<caja> a )
+/*
+* @return regresa el menor de la lista 
+*/
+cajero getMenorT( list<cajero> a )
 {
-    caja resultado(-1, -1);
-    for (std::list<caja>::iterator it=a.begin(); it != a.end(); ++it){
-        int tmp = resultado.getTiempoEspera();
-        caja tmp3 = *it;
+    cajero resultado(-1, -1); // crear un objeto vac
+    for (std::list<cajero>::iterator it=a.begin(); it != a.end(); ++it){
+        int tmp = resultado.getTiempoEspera(); // asigno valor a 
+        cajero tmp3 = *it;
         if(tmp != -1){
             int tmp1 = tmp3.getTiempoEspera();
             cout<<"El tiempo de espera para " << tmp3.getId() << " es igual a "<< tmp3.getTiempoEspera()<<endl;
@@ -227,71 +220,41 @@ caja getMenorT( list<caja> a )
 }
 
 
-
-list<caja> acomoda( list<caja> caj, list<cliente> clis, cliente cli)
+/*
+* @return
+*/
+list<cajero> acomoda( list<cajero> caj, list<cliente> clis, cliente cli)
 {
-    caja tmp = getMenorT(caj);
+    cajero tmp = getMenorT(caj);
     int id = tmp.getId();
     int a = tmp.getId();
     int b = cli.getId();
     int f = std::stoi(cli.getTiempoOp());
-    for (std::list<caja>::iterator it=caj.begin(); it != caj.end(); ++it)
+    for (std::list<cajero>::iterator it=caj.begin(); it != caj.end(); ++it)
     {
       if((*it).getId() == id){
         (*it).add(cli);
         (*it).masT(f);
       }
     }
-    cout<<"Se agrega el cliente con id "<< b << " en la caja con id " << a << " se añade " << f << " segundos al tiempo de espera"  <<endl;
+    cout<<"Se agrega el cliente con id "<< b << " en la cajero con id " << a << " se añade " << f << " segundos al tiempo de espera"  <<endl;
     return caj;
-    /*switch ( cli.getTipoCliente() )
-                          {
-                             case "P":
-                                acomodaP(caj, clis, cli);
-                                break;
-                             case "C":
-                                acomodaC(caj, clis, cli);
-                                break;
-                             case "N":
-                                acomodaN(caj, clis, cli);
-                             default:
-                                cout<<"Error"<<endl;
-                                exit(1);
-                          }*/
+    
 
 }
 
-/*void acomodaP( list<caja> caj, list<cliente> clis, cliente cli){
-    for (std::list<string>::iterator it=caj.begin(); it != caj.end(); ++it){
-        //int esp = it*.getTiempoEspera();
-        if((it*.getU()).empty()){
-            it*.add(cli);
-            it*.masT(cli.getTiempoOp());
-        }
-    }
-    caja min = getMenorT(caj);
-    if(min.getTiempoEspera() < 6000){
-        min.add(cli);
-    }else{
-        acomodaD("P", min, cli);
-    }
 
-}
 
-void acomodaC( list<caja> caj, list<cliente> clis, cliente cli){
-
-}
-
-void acomodaN( list<caja> caj, list<cliente> clis, cliente cli){
-
-}*/
-
+/*
+*  tomar 
+* @return
+*/
 cliente getPrimero( list<cliente> a )
 {
-    int menor = 86399;
+    
     cliente resultado;
-    for (std::list<cliente>::iterator it=a.begin(); it != a.end(); ++it){
-        cliente tmp = *it;
+    for (std::list<cliente>::iterator it=a.begin(); it != a.end(); ++it){ // recorido de los clientes 
+        cliente tmp = *it; // tomar un temporal 
         int b = std::stoi(tmp.getHoraLlegada());
         int c = resultado.getId();
         if(c != -1){
@@ -337,7 +300,7 @@ cliente getPrimero( list<cliente> a )
 
  /**
  * hora de llegada del cliente a segundos
- *
+ * @return
  */
 int sacarSegundosDeHora(string hora ){
   string hoora= hora.substr (0,2);
@@ -370,55 +333,12 @@ int personaColaSeg =atoi(seg.c_str());
 
 }
 
-int sacarSegundosSalida(string b ){
-
-  string hoora= b.substr (19,2);
-  string min = b.substr (22,2);
-  string seg = b.substr (25,2);
-  //cambiamos a numero
-  int temHora = atoi(hoora.c_str());
-  int personaColaMin =atoi(min.c_str());
-  int personaColaSeg =atoi(seg.c_str());
-  personaColaMin = (temHora*60)+personaColaMin;
-   //cout<<"segunf: "<<personaColaSeg<<endl;
-   return personaColaSeg = (personaColaMin*60)+personaColaSeg;
-}
 
 
 /*
-* saca el timepo a string para guardar en la lista
-* @ seg total de segundos
-* @ string hora hora en string
+*
+* man 
 */
-string sacarHoraDeSegundos(int seg){
-  string hora ;
-  int horas = seg/3600;
-  int minutos= (seg-(3600*horas))/60;
-  int segundos =seg-((horas*3600)+(minutos*60));
-  string h =to_string(horas);
-  string m=to_string(minutos);;
-  string s=to_string(segundos);;
-  if(horas<10){
-    h="0";
-    string t= to_string(horas);
-    h+=t;
-  } 
-  if(minutos<10){
-    m="0";
-    string t= to_string(minutos);
-    m+=t;
-  }
-  if(segundos<10){
-    s="0";
-    string t= to_string(segundos);
-    s+=t;
-  }
-  //cout<<h<<"-"<<m<<"-"<<s<<endl;
-  hora =h+":"+m+":"+s;
-  return hora;
-  
-
-}
 
 int main(int argc, char **argv) {
   // atributos 
@@ -429,7 +349,7 @@ int main(int argc, char **argv) {
     string arr[4];
     string frase;
     list<cliente> clientes;
-    list<caja> cajeros ;
+    list<cajero> cajeros ;
     
     //10 min = 600 s
     // 30 min 1800 s
@@ -459,6 +379,7 @@ int main(int argc, char **argv) {
             std::string::size_type e = i.find(",");
             while(e != std::string::npos){
               string a = i.substr(0, e);
+              cout << a <<endl;
               i = i.erase(0, e+1);
               v.push_back(a);
               e = i.find(",");
@@ -477,26 +398,14 @@ int main(int argc, char **argv) {
     * agrego a la lista  los primeros elentos
     */
     for (int i = 0; i < numCajeros; i++){ 
-        caja ca(i, 0);
+        cajero ca(i, 0);
         cajeros.push_back(ca);
-        /*string a = clientes.front();// persona 
-        int id = sacarId(a);
-        int timepoLLegada = sacarSegundosDeHora(a); 
-        int timpoTarda = sacarSegundosTiempo(a);
-        string tipoCliente = sacarTipoCliente(a);      
-        /*ca.setIdCajero(i); // agrego id 
         
-        
-        int tiempoSalida = timepoLLegada +timpoTarda;
-        a+="-"+sacarHoraDeSegundos(tiempoSalida);
-        ca.add(a);
-        cout<<"per:"<<a<<endl;
-        cajeros.push_front(ca); // meto un elemtno a  un cajero
-        
-        clientes.pop_front(); // elimino el ultimo , actilizo la lista*/
         
     }
-
+    /*
+    * recorro la lista de clientes 
+    */
     while(!(clientes.empty())){
         cliente primero = getPrimero( clientes );
         clientes.remove(primero);
@@ -504,76 +413,7 @@ int main(int argc, char **argv) {
         cout<< primero.getTipoCliente() <<endl;
     }
     cout<<"tamño cajeros : "<< cajeros.size()<<endl;
-    /*caja a = cajeros.pop_front();
-    list<cliente> b = a.getU();
-    cliente c = b.pop_front();
-    cliente d = b.pop_back();
-    int e = c.getId();
-    int f = d.getId();
-    cout<< e <<endl;
-    cout<< f <<endl;
-
-    /**
-    *  intentar recorrer
-    */
     
-    /*
-    * acomodar por cajeros
-    *
-    **/
-
-
-    /*list<string>::iterator it =clientes.begin();
-    
-
-
-    while(it != clientes.end() ){ // clientes se acaben  n 
-      // *it  = valor iterador
-      //list<string> l;
-      //cout <<"s: "<< *it <<endl;
-      list<cajero>::iterator itCajeros = cajeros.begin();
-      while( itCajeros != cajeros.end()){ // recorrer numero cajeros m
-            /**
-            *
-            *   datos de cliente de la cola
-            */
-            /*string personaCola = *it;
-            int timepoLLegada = sacarSegundosDeHora(personaCola);
-            int timpoMax =timepoLLegada-1; // mas una hora
-            int timpoTarda = sacarSegundosTiempo(personaCola);
-            string p = personaCola.substr(2,1); 
-            if(p=="P"){
-              timpoMax += 600;  
-            }
-            if(p== "C"){
-              timpoMax += 1800;
-            }
-            /*
-            * datos de el ultimo que esta usando el cajero
-            */
-            /*cajero te = *itCajeros;
-            string x = te.getU(); // saca el ulitmo de del enfrete de la lista
-            int ultimoSale = sacarSegundosSalida(x);
-
-              
-            cout <<"per: " <<x << " tarda en salir: " <<sacarHoraDeSegundos(ultimoSale)<<endl;
-              //cout<<ultimoSale<<">"<<timpoMax<<endl;
-            if(timpoMax > ultimoSale){
-
-                        
-            }else{
-              
-            }
-            
-        
-        itCajeros++;*/
-      //}
-      //it++; // incrementa el iterador 
-    //}
-
-    
-    
-    //cout << nombreFichero<<endl;
   
   return 0;
 }
